8666d34b1ffb52bc3de2e897cb2d2c5252dbf393
aclevi88 <47267222+aclevi88@users.noreply.github.com>
2024-09-22 20:38:18 -0400

    Initial commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/in/ambrose-levi-28941a42"
+        > Ambrose Levi</a
+      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Firebase
+    </p>
+  </div>
+  );
+}
